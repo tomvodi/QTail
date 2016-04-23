658a96f0d0d3e7ca1fd1e8abf81a148f33aab593
Thomas Baumann <teebaum@ymail.com>
2016-04-23 15:28:08 +0200

    added FileListItemWidget test

--- src/gui/FileListItemWidget.h
+++ src/gui/FileListItemWidget.h
@@ -17,10 +17,25 @@ class FileListItemWidget;
 class FileListItemWidget : public QFrame
 {
     Q_OBJECT
+    friend class FileListItemWidgetTest;
 
 public:
+    enum class FileState {
+        FileError,
+        FileInSync,
+        FileHasNewLines
+    };
+
     explicit FileListItemWidget(QWidget *parent = 0);
     ~FileListItemWidget();
+
+    void setFileName(const QString &fileName);
+    void setLineCount(quint64 lineCount);
+
+    void setFileState(FileState state);
+
+signals:
+    void closeFileRequested();
 
 private:
     Ui::FileListItemWidget *ui;
---
+++ tests/gui/FileListItemWidget/tst_FileListItemWidgetTest.cpp
@@ -0,0 +1,63 @@
+/**
+ * @author  Thomas Baumann <teebaum@ymail.com>
+ *
+ * @section LICENSE
+ * See LICENSE for more informations.
+ *
+ */
+
+#include <QString>
+#include <QtTest>
+#include <QCoreApplication>
+
+#include <gui/FileListItemWidget.h>
+#include <ui_FileListItemWidget.h>
+
+class FileListItemWidgetTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    FileListItemWidgetTest();
+
+private Q_SLOTS:
+    void initTestCase();
+    void cleanupTestCase();
+    void testSetFileName();
+    void testSetLineCount();
+};
+
+FileListItemWidgetTest::FileListItemWidgetTest()
+{
+}
+
+void FileListItemWidgetTest::initTestCase()
+{
+}
+
+void FileListItemWidgetTest::cleanupTestCase()
+{
+}
+
+void FileListItemWidgetTest::testSetFileName()
+{
+    QString testText("blabla.txt");
+    FileListItemWidget listWidget;
+    listWidget.setFileName(testText);
+
+    QVERIFY2(listWidget.ui->fileNameLabel->text() == testText, "Filename wasn't set in gui");
+}
+
+void FileListItemWidgetTest::testSetLineCount()
+{
+    quint64 lineCount = 45312;
+    FileListItemWidget listWidget;
+    listWidget.setLineCount(lineCount);
+
+    QVERIFY2(listWidget.ui->lineCountLabel->text() == QString::number(lineCount),
+             "Line count wasn't set in gui");
+}
+
+QTEST_MAIN(FileListItemWidgetTest)
+
+#include "tst_FileListItemWidgetTest.moc"
