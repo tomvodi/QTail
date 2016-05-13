a687dc2cd9b690e820952f6629b90524cf22b984
Thomas Baumann <teebaum@ymail.com>
2016-05-13 15:48:48 +0200

    highlight current line for read only text edit

--- src/gui/file_views/PlainTextEdit.cpp
+++ src/gui/file_views/PlainTextEdit.cpp
@@ -82,17 +82,15 @@ void PlainTextEdit::highlightCurrentLine()
 {
    QList<QTextEdit::ExtraSelection> extraSelections;
 
-   if (!isReadOnly()) {
-      QTextEdit::ExtraSelection selection;
+   QTextEdit::ExtraSelection selection;
 
-      QColor lineColor = QColor(Qt::yellow).lighter(160);
+   QColor lineColor = QColor(Qt::yellow).lighter(160);
 
-      selection.format.setBackground(lineColor);
-      selection.format.setProperty(QTextFormat::FullWidthSelection, true);
-      selection.cursor = textCursor();
-      selection.cursor.clearSelection();
-      extraSelections.append(selection);
-   }
+   selection.format.setBackground(lineColor);
+   selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+   selection.cursor = textCursor();
+   selection.cursor.clearSelection();
+   extraSelections.append(selection);
 
    setExtraSelections(extraSelections);
 }
