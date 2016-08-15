f79fb1d6a238913f2bb141651d66993f9b527f4b
Thomas Baumann <teebaum@ymail.com>
2016-08-15 10:30:56 +0200

    added translations

---
+++ src/gui/translations/QTail_de.ts
@@ -0,0 +1,380 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../AboutDialog.ui" line="14"/>
+        <source>About QTail</source>
+        <translation>Über QTail</translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.ui" line="55"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/chekavy/cheser-icon-theme&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#007af4;&quot;&gt;Cheezer icon theme&lt;/span&gt;&lt;/a&gt; is licenced under the Creative Commons Attribution-Share Alike 3.0 United States License (see &lt;a href=&quot;http://creativecommons.org/licenses/by-sa/3.0/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#007af4;&quot;&gt;http://creativecommons.org/licenses/by-sa/3.0/&lt;/span&gt;&lt;/a&gt;).&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FileFilterWidget</name>
+    <message>
+        <location filename="../filter/FileFilterWidget.ui" line="62"/>
+        <source>Apply File Filters</source>
+        <translation>Filter anwenden</translation>
+    </message>
+</context>
+<context>
+    <name>FilterDialog</name>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="14"/>
+        <source>Filter Dialog</source>
+        <translation>Filter Dialog</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="94"/>
+        <source>Add filter rule</source>
+        <translation>Filterregel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="97"/>
+        <source>Add Rule</source>
+        <translation>Regel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="124"/>
+        <source>Delete selected filter rule</source>
+        <translation>Markierte Filterregel löschen</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="127"/>
+        <source>Delete Rule</source>
+        <translation>Regel löschen</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="165"/>
+        <source>Filter group:</source>
+        <translation>Filtergruppe:</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="175"/>
+        <source>Add new filter group</source>
+        <translation>Neue Filtergruppe hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="230"/>
+        <source>Filter Definition</source>
+        <translation>Filter-Definition</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="236"/>
+        <source>Case Sensitive</source>
+        <translation>Groß-Kleinschreibung beachten</translation>
+    </message>
+    <message>
+        <location filename="../filter/FilterDialog.ui" line="246"/>
+        <source>String / Regular Expression:</source>
+        <translation>Zeichenkette / Regulärer Ausdruck:</translation>
+    </message>
+</context>
+<context>
+    <name>FontPicker</name>
+    <message>
+        <location filename="../highlighting/FontPicker.ui" line="41"/>
+        <source>pt</source>
+        <translation>pt</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/FontPicker.ui" line="57"/>
+        <source>Bold</source>
+        <translation>Fett</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/FontPicker.ui" line="71"/>
+        <source>Italic</source>
+        <translation>Kursiv</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/FontPicker.ui" line="85"/>
+        <source>Underline</source>
+        <translation>Unterstrichen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/FontPicker.ui" line="99"/>
+        <source>Strikeout</source>
+        <translation>Durchgestrichen</translation>
+    </message>
+</context>
+<context>
+    <name>HighlightingDialog</name>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="14"/>
+        <source>Highlighting Dialog</source>
+        <translation>Hervorhebungen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="67"/>
+        <source>Add highlighting rule</source>
+        <translation>Hervorhebung hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="70"/>
+        <source>Add Rule</source>
+        <translation>Regel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="97"/>
+        <source>Move rule to top</source>
+        <translation>Regel nach ganz oben verschieben</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="100"/>
+        <source>Top</source>
+        <translation>Nach ganz oben</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="114"/>
+        <source>Move rule upwards</source>
+        <translation>Regel nach oben verschieben</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="117"/>
+        <source>Up</source>
+        <translation>Nach oben</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="147"/>
+        <source>Move rule downwards</source>
+        <translation>Regel nach unten verschieben</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="150"/>
+        <source>Down</source>
+        <translation>Nach unten</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="161"/>
+        <source>Move rule to bottom</source>
+        <translation>Regel nach ganz unten verschieben</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="164"/>
+        <source>Bottom</source>
+        <translation>Unten</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="188"/>
+        <source>Delete selected highlighting rule</source>
+        <translation>Ausgewählte Regel löschen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="191"/>
+        <source>Delete Rule</source>
+        <translation>Regel löschen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="208"/>
+        <source>Rule Definition</source>
+        <translation>Regel-Definitionen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="216"/>
+        <source>Foreground Color:</source>
+        <translation>Vordergrundfarbe:</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="233"/>
+        <source>Background Color:</source>
+        <translation>Hintergrundfarbe:</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="250"/>
+        <source>Font:</source>
+        <translation>Schrift:</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="269"/>
+        <source>String / Regular Expression:</source>
+        <translation>Zeichenkette / Regulärer Ausdruck:</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="279"/>
+        <source>Case Sensitive</source>
+        <translation>Groß-Kleinschreibung beachten</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="309"/>
+        <source>Word highlight rules</source>
+        <translation>Wort-Hervorhebungen</translation>
+    </message>
+    <message>
+        <location filename="../highlighting/HighlightingDialog.ui" line="348"/>
+        <source>Line highlight rules</source>
+        <translation>Zeilen-Hervorhebungen</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.ui" line="59"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="63"/>
+        <source>Recent files</source>
+        <translation>Kürzlich geöffnet</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="72"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="78"/>
+        <source>&amp;View</source>
+        <translation>&amp;Ansicht</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="82"/>
+        <source>Windows</source>
+        <translation>Fenster</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="91"/>
+        <source>&amp;Edit</source>
+        <translation>B&amp;earbeiten</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="121"/>
+        <location filename="../MainWindow.ui" line="225"/>
+        <source>Open Files</source>
+        <translation>Dateien öffnen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="151"/>
+        <location filename="../MainWindow.ui" line="254"/>
+        <source>File Filters</source>
+        <translation>Dateifilter</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="182"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Beenden</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="191"/>
+        <source>&amp;Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="194"/>
+        <source>Ctrl+O</source>
+        <translation>Strg+O</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="199"/>
+        <source>About QTail</source>
+        <translation>Über QTail</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="208"/>
+        <source>Highlighting</source>
+        <translation>Hervorhebungen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="211"/>
+        <source>Highlight </source>
+        <translation>Hervorhebung</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="214"/>
+        <source>Alt+G</source>
+        <translation>Alt+G</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="234"/>
+        <source>&amp;Preferences</source>
+        <translation>Ein&amp;stellungen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="243"/>
+        <source>Filter</source>
+        <translation>Filter</translation>
+    </message>
+</context>
+<context>
+    <name>PlainTextViewWidget</name>
+    <message>
+        <location filename="../file_views/PlainTextViewWidget.ui" line="73"/>
+        <source>Navigate to bottom of file</source>
+        <translation>Zum Ende der Datei navigieren</translation>
+    </message>
+    <message>
+        <location filename="../file_views/PlainTextViewWidget.ui" line="76"/>
+        <source>Navigate to bottom</source>
+        <translation>Zum Ende der Datei navigieren</translation>
+    </message>
+    <message>
+        <location filename="../file_views/PlainTextViewWidget.ui" line="106"/>
+        <source>Navigate to current line</source>
+        <translation>Zur aktuellen Zeile navigieren</translation>
+    </message>
+    <message>
+        <location filename="../file_views/PlainTextViewWidget.ui" line="109"/>
+        <source>Navigate to cursor</source>
+        <translation>Zum Cursor navigieren</translation>
+    </message>
+    <message>
+        <location filename="../file_views/PlainTextViewWidget.ui" line="126"/>
+        <source>Navigate to top of file</source>
+        <translation>Zum Anfang der Datei navigieren</translation>
+    </message>
+    <message>
+        <location filename="../file_views/PlainTextViewWidget.ui" line="129"/>
+        <source>Navigate to top</source>
+        <translation>Zum Anfang der Datei navigieren</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="14"/>
+        <source>Preferences</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="23"/>
+        <source>The default font used in the text view</source>
+        <translation>Die Standardschrift für die Textansicht</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="26"/>
+        <source>Font:</source>
+        <translation>Schrift:</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="64"/>
+        <source>Text View</source>
+        <translation>Textansicht</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="81"/>
+        <source>Wrap lines at right edge of text view</source>
+        <translation>Zeilenumbruch an der rechten Seite der Textansicht</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="84"/>
+        <source>Wrap lines:</source>
+        <translation>Zeilen umbrechen:</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="94"/>
+        <source>Update interval of the text view</source>
+        <translation>Zeitintervall für die Aktualisierung der Textansicht</translation>
+    </message>
+    <message>
+        <location filename="../PreferencesDialog.ui" line="97"/>
+        <source>Update interval:</source>
+        <translation>Zeitintervall für Aktualisierung:</translation>
+    </message>
+</context>
+</TS>
