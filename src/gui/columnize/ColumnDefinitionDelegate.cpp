06bb4f0b39d047e818b691a1af5c0487631d4256
Thomas Baumann <teebaum@ymail.com>
2016-09-03 16:20:43 +0200

    finished delegate

--- src/gui/columnize/ColumnDefinitionDelegate.cpp
+++ src/gui/columnize/ColumnDefinitionDelegate.cpp
@@ -13,6 +13,10 @@
 #include "ColumnDefinitionInterface.h"
 #include "ColumnDefinitionDelegate.h"
 
+static qreal PaddingLeftRight(0.6); // Item padding left and right
+static qreal PaddingTopBottom(0.4); // Item padding top and bottom
+static qreal Spacing(0.25);          // Spacing between text lines
+
 ColumnDefinitionDelegate::ColumnDefinitionDelegate(QObject *parent)
    : QStyledItemDelegate(parent)
 {
@@ -31,8 +35,28 @@ void ColumnDefinitionDelegate::paint(QPainter *painter, const QStyleOptionViewIt
       return;
    }
 
-   QRect nameRect = option.rect;
-   painter->drawText(nameRect, Qt::AlignLeft, definition->name());
+   QFontMetrics metrics(option.font);
+
+   QRectF nameRect = option.rect;
+   QRect charBoundingRect = metrics.boundingRect("9");
+   qreal paddingLR = PaddingLeftRight * charBoundingRect.width();
+   qreal paddingTopBot = PaddingTopBottom * charBoundingRect.height();
+   nameRect.adjust(paddingLR, paddingTopBot, -paddingLR, 0);
+   painter->save();
+   QFont nameFont(option.font);
+   nameFont.setBold(true);
+   painter->setFont(nameFont);
+   painter->drawText(nameRect, Qt::AlignLeft | Qt::AlignTop, definition->name());
+   painter->restore();
+
+   nameRect.adjust(0, charBoundingRect.height() + Spacing * charBoundingRect.height(), 0, 0);
+
+   QFont descriptionFont(option.font);
+   descriptionFont.setItalic(true);
+   painter->save();
+   painter->setFont(descriptionFont);
+   painter->drawText(nameRect, Qt::AlignLeft | Qt::AlignTop, definition->description());
+   painter->restore();
 }
 
 QSize ColumnDefinitionDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
@@ -46,9 +70,16 @@ QSize ColumnDefinitionDelegate::sizeHint(const QStyleOptionViewItem &option, con
    }
 
    QFontMetrics metrics(option.font);
-   int height = 2 * metrics.boundingRect("9").height();
+   QRect boundingRectChar = metrics.boundingRect("9");
+   int height = 2 * boundingRectChar.height() +
+                Spacing * boundingRectChar.height() +
+                2 * PaddingTopBottom * boundingRectChar.height();
    size.setHeight(height);
-   size.setWidth(200);
+
+   int maxWidth = qMax(metrics.boundingRect(definition->description()).width(),
+                       metrics.boundingRect(definition->name()).width());
+   maxWidth += 2 * PaddingLeftRight * boundingRectChar.width();
+   size.setWidth(maxWidth);
 
    return size;
 }
